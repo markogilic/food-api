762ecf4c06e94a1db0be5210817dd79cb6e54a00
Elliot Evans <ell15evans.nuls@googlemail.com>
2023-09-09 19:50:14 +0300

    fix(): removed auth folder as it is not needed

--- src/auth/context/auth-context.tsx
+++
@@ -1,8 +0,0 @@
-// REACT
-import { createContext } from 'react';
-
-// TYPES
-import { JWTContextType } from './types';
-
-// ---------------------------------------------------------------------------
-export const AuthContext = createContext({} as JWTContextType);
