93e84aac1da4b2e1e14fc1fd8e1d3263a8b30c00
MArko Gilic <gilicmarko10@gmail.com>
2023-09-13 11:21:22 +0200

    strugling with some basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const queryClient = new QueryClient();
+const imagePath = 'assets/';
+const extenstion = '.jpeg';
 
 const App = () => {
+   const [items, setItems] = useState([]);
+   const [veg, setVeg] = useState({} as any);
+   useEffect(() => {
+      axios
+         .get('http://localhost:3500/food')
+         .then((res) => setItems(res.data))
+         .catch((err) => console.log(err));
+   }, []);
+   console.log(items);
+   useEffect(() => {
+      setVeg(items.veg);
+   }, []);
+   console.log(veg);
    return (
-         <QueryClientProvider client={queryClient}>
-            <main>
-               Food API
-            </main>
-         </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+         <main>
+            {/* {veg.map((item: any) => (
+               <div key={item.id}>
+                  <h2>{item.title}</h2>
+                  <img src={imagePath + item.slug + extenstion} alt={item.slug} />
+               </div>
+            ))} */}
+         </main>
+      </QueryClientProvider>
    );
 };
 
