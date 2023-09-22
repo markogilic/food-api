f0be921693ad3f671cb1e45ecb186890c3841cc9
MArko Gilic <gilicmarko10@gmail.com>
2023-09-22 22:13:44 +0200

    last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-// import ShowRanFood from './component/ShowRanFood';
+import ShowRanFood from './component/ShowRanFood';
 
 const queryClient = new QueryClient();
-// const imagePath = 'assets/';
-// const extenstion = '.jpeg';
 
 const App = () => {
    const [foods, setFood] = useState({} as any);
    // const [veg, setVeg] = useState([] as any);
    // const [fruit, setFruit] = useState([] as any);
+   const [allFood, setAllFood] = useState([] as any);
    useEffect(() => {
       axios
          .get('http://localhost:3500/food')
@@ -20,15 +19,32 @@ const App = () => {
    // console.log(items.veg);
    // const veg = items.veg;
    // console.log(items.food.veg);
-   // useEffect(() => {
-   //    setFruit(items.fruit);
-   // }, []);
+   useEffect(() => {
+      if (Object.keys(foods).length !== 0) {
+         // setFruit(foods.fruit);
+         // setVeg(foods.veg);
+         foods.fruit.map((item: any) => {
+            setAllFood((prev: any) => [...prev, item.slug]);
+         });
+         foods.veg.map((item: any) => {
+            setAllFood((prev: any) => [...prev, item.slug]);
+         });
+      }
+   }, [foods]);
    // console.log(items.fruit);
-   console.log(foods.veg);
+   // console.log(foods);
+   // console.log(Object.keys(foods).length !== 0);
+   // console.log(fruit);
+   // const chekObject = function (obj: any) {
+   //    return Object.keys(obj).length !== 0;
+   // };
+
+   // console.log(fruit[0].title);
 
    return (
       <QueryClientProvider client={queryClient}>
          <main>
+            {allFood.length !== 0 && <ShowRanFood allFood={allFood} />}
             {/* {fruit?.map((item: any) => (
                <div key={item.id}>
                   <h2>{item.title}</h2>
@@ -37,10 +53,25 @@ const App = () => {
             ))} */}
             {/* {Object.keys(foods).length !== 0 && <ShowRanFood {{veg,fruit}: foods} />} */}
             {/* {veg && <ShowRanFood veg={veg} />} */}
-            {foods &&
+            {/* {foods &&
                foods.veg.map((item: any) => {
                   return <p key={item.id}>{item.title}</p>;
-               })}
+               })} */}
+            {/* {chekObject(foods) &&
+               foods.veg.map((item: any) => {
+                  return (
+                     <div key={item.id}>
+                        <h2>{item.title}</h2>
+                        <img src={imagePath + item.slug + extenstion} alt={item.slug} />
+                     </div>
+                  );
+               })} */}
+            {/* {veg?.map((item: any) => (
+               <div key={item.id}>
+                  <h2>{item.title}</h2>
+                  <img src={imagePath + item.slug + extenstion} alt={item.slug} />
+               </div>
+            ))} */}
          </main>
       </QueryClientProvider>
    );
--- src/component/ShowRanFood.tsx
+++ src/component/ShowRanFood.tsx
@@ -1,16 +1,32 @@
-function ShowRanFood(veg: any) {
-   console.log(veg);
+import { useEffect, useState } from 'react';
+
+function ShowRanFood({ allFood }: { allFood: any[] }) {
+   const imagePath = 'assets/';
+   const extenstion = '.jpeg';
+   const [ranNumber, setRanNumber] = useState(0);
+
+   const getRandomInt = (max: number) => {
+      return Math.floor(Math.random() * max);
+   };
+   useEffect(() => {
+      setRanNumber(getRandomInt(allFood.length));
+   }, []);
+   console.log(ranNumber);
+   console.log(allFood[ranNumber]);
    return (
       <div>
+         <h1>Random Food</h1>
+         <img src={imagePath + allFood[ranNumber] + extenstion} />
+         {/* <img src={imagePath + allFood[getRandomInt].slug + extenstion} alt={item.slug} /> */}
          {/* <h1>Random Fruit</h1>
          {fruit.map((item: any) => {
             return <p key={item.id}>{item}</p>;
          })} */}
 
-         <h1>Random Veg</h1>
+         {/* <h1>Random Veg</h1>
          {veg.map((item: any) => {
             return <p key={item.id}>{item}</p>;
-         })}
+         })} */}
       </div>
    );
 }
